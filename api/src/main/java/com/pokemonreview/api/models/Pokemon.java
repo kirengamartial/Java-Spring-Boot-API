15f7b0294e888214304b1e86c19a248e77040b83
kirengamartial <kirengamartial@gmail.com>
2024-11-12 21:48:45 +0200

    first java spring boot application api

---
+++ api/src/main/java/com/pokemonreview/api/models/Pokemon.java
@@ -0,0 +1,25 @@
+package com.pokemonreview.api.models;
+
+import lombok.Data;
+
+@Data
+public class Pokemon {
+    private int id;
+    private String name;
+    private String type;
+
+//    public int getId() {
+//        return id;
+//    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+
+//    public Pokemon(int id, String name, String type) {
+//        this.id = id;
+//        this.name = name;
+//        this.type = type;
+//    }
+
+
+}
