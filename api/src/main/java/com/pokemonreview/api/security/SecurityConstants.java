6b0337e0f1f39780add7fa5b7820d73583f571cb
Kirenga Martial <77124756+kirengamartial@users.noreply.github.com>
2025-05-22 20:35:08 +0200

    Update SecurityConstants.java

--- api/src/main/java/com/pokemonreview/api/security/SecurityConstants.java
+++ api/src/main/java/com/pokemonreview/api/security/SecurityConstants.java
@@ -2,5 +2,5 @@ package com.pokemonreview.api.security;
 
 public class SecurityConstants {
     public static final long JWT_EXPIRATION = 86400000;
-    public static final String JWT_SECRET = "secret";
+    public static final String JWT_SECRET
 }
